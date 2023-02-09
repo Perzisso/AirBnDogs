f63fa67ffea0b736d16712935dd0f518bbe0a329
Florian <florian.sasso@outlook.fr>
2023-02-09 20:39:33 +0100

    seed added

--- app/models/city.rb
+++ app/models/city.rb
@@ -1,5 +1,4 @@
 class City < ApplicationRecord
     has_many :dogsitter
     has_many :dog
-    has_many :strolls
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,30 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Dog.destroy_all
+Dogsitter.destroy_all
+City.destroy_all
+
+10.times do
+    city = City.create!(city_name: Faker::Nation.capital_city)
+  end
+
+  10.times do
+    dogsitter = Dogsitter.create!(first_name: Faker::Name.first_name,
+                        city_id: rand(1..10))
+end
+
+10.times do
+    dog = Dog.create!(race: Faker::Creature::Dog.breed,
+                        city_id: rand(1..10))
+end
+
+10.times do
+    stroll = Stroll.create(dogsitter_id: rand(1..10),
+                            dog_id: rand(1..10),
+                            city_id: rand(1..10))
+end
+
+
+
